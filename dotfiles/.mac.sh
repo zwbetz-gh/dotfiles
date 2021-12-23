b522596c39ba28cde66ce422306881c3c8ddce5a
zwbetz-gh <zwbetz@gmail.com>
2021-12-22 20:27:34 -0600

    nodejs

--- dotfiles/.mac.sh
+++ dotfiles/.mac.sh
@@ -9,6 +9,8 @@ source ${HOME}/.mac_various.sh
 # Env
 ################################################################################
 export JAVA_HOME=${HOME}/bin/java/jdk-11.0.13+8/Contents/Home
+export NODE_HOME=${HOME}/bin/node/node-v16.13.1-darwin-arm64
+
 # export LD_LIBRARY_PATH=${HOME}/bin/oracle/instantclient_19_3:${LD_LIBRARY_PATH}
 # export POSTGRES_HOME=/Applications/Postgres.app/Contents/Versions/latest
 # export DOTNET_HOME=${HOME}/bin/dotnet-sdk-3.1.101-osx-x64
@@ -21,10 +23,13 @@ export JAVA_HOME=${HOME}/bin/java/jdk-11.0.13+8/Contents/Home
 # PATH
 ################################################################################
 export PATH=${JAVA_HOME}/bin:${PATH}
+export PATH=${NODE_HOME}/bin:${PATH}
+
 # export PATH=${LD_LIBRARY_PATH}:${PATH}
 # export PATH=${POSTGRES_HOME}/bin:${PATH}
 # export PATH=${DOTNET_HOME}:${PATH}
 # export PATH=${DOTNET_TOOLS_HOME}:${PATH}
 # export PATH=${FLYWAY_HOME}:${PATH}
 # export PATH=${LOGSTASH_HOME}/bin:${PATH}
+
 export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
