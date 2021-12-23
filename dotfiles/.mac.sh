40ddc9412e827425798c216c36647d5abd98a6ff
zwbetz-gh <zwbetz@gmail.com>
2021-12-22 20:30:58 -0600

    hugo

--- dotfiles/.mac.sh
+++ dotfiles/.mac.sh
@@ -10,6 +10,7 @@ source ${HOME}/.mac_various.sh
 ################################################################################
 export JAVA_HOME=${HOME}/bin/java/jdk-11.0.13+8/Contents/Home
 export NODE_HOME=${HOME}/bin/node/node-v16.13.1-darwin-arm64
+export HUGO_HOME=${HOME}/bin/hugo/hugo_extended_0.91.1_macOS-ARM64
 
 # export LD_LIBRARY_PATH=${HOME}/bin/oracle/instantclient_19_3:${LD_LIBRARY_PATH}
 # export POSTGRES_HOME=/Applications/Postgres.app/Contents/Versions/latest
@@ -24,6 +25,7 @@ export NODE_HOME=${HOME}/bin/node/node-v16.13.1-darwin-arm64
 ################################################################################
 export PATH=${JAVA_HOME}/bin:${PATH}
 export PATH=${NODE_HOME}/bin:${PATH}
+export PATH=${HUGO_HOME}:${PATH}
 
 # export PATH=${LD_LIBRARY_PATH}:${PATH}
 # export PATH=${POSTGRES_HOME}/bin:${PATH}
