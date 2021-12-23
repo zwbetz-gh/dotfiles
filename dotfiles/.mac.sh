9ee990798d6158e7abde926f1f6da9ad112d8f37
zwbetz-gh <zwbetz@gmail.com>
2021-12-23 14:36:52 -0600

    add scripts to path

--- dotfiles/.mac.sh
+++ dotfiles/.mac.sh
@@ -12,15 +12,15 @@ source ${HOME}/.mac_various.sh
 
 export JAVA_HOME=${HOME}/bin/java/jdk-11.0.13+8/Contents/Home
 export NODE_HOME=${HOME}/bin/node/node-v16.13.1-darwin-arm64
-export HUGO_HOME=${HOME}/bin/hugo/hugo_extended_0.91.1_macOS-ARM64
 
 ################################################################################
 # PATH
 ################################################################################
 
+export PATH=${HOME}/bin/scripts:${PATH}
 export PATH=${JAVA_HOME}/bin:${PATH}
 export PATH=${NODE_HOME}/bin:${PATH}
-export PATH=${HUGO_HOME}:${PATH}
+export PATH=${HOME}/bin/hugo/hugo_extended_0.91.1_macOS-ARM64:${PATH}
 
 # Add `code`
 export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
