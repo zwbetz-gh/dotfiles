93078f4bb7ec6ad80f176aade459e486ed773e36
zwbetz-gh <zwbetz@gmail.com>
2021-12-22 22:05:05 -0600

    fix jots on new machine

--- dotfiles/.common_jots.sh
+++ dotfiles/.common_jots.sh
@@ -4,6 +4,7 @@ JOTS_FILE="${JOTS_DIR}/jots.txt"
 bfl_jots() {
   local TODAY="$(date +'%F')"
   local DATESTAMP="DATESTAMP ${TODAY}"
+  mkdir -p ${JOTS_DIR}
   touch ${JOTS_FILE}
   if ! grep "${DATESTAMP}" ${JOTS_FILE} > /dev/null; then
     echo "${DATESTAMP} does not exist, stamping"
