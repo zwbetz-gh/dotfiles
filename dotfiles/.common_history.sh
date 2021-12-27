e9487499b424ebb21ba32a8df37bb294f86e74a9
zwbetz-gh <zwbetz@gmail.com>
2021-12-27 09:40:41 -0600

    remove unused

--- dotfiles/.common_history.sh
+++ dotfiles/.common_history.sh
@@ -7,9 +7,5 @@
 # Avoid duplicates in history file
 export HISTCONTROL=ignoredups:erasedups
 
-# Allow for big history file size
-# export HISTSIZE=100000
-# export HISTFILESIZE=100000
-
 # When shell exits, append to history file instead of overwriting it
 shopt -s histappend
